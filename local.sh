52f1821638f66d73a78efe99c0e8ecff65fc4330
Greg Solomon <greg.solomon@capgemini.com>
2025-08-22 08:10:13 +0100

    Tidy ups

--- local.sh
+++ local.sh
@@ -1,2 +1,8 @@
+clear
 . ./venv/bin/activate
+echo ""
+echo ""
+echo "                            Access the app at http://localhost:8000"
+echo ""
+echo ""
 gunicorn app:app --bind 0.0.0.0:8000 --workers 2
